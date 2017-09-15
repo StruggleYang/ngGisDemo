51cefdfded3a46368dd6e313c51b21b29a50dd8e
StruggleYang <1724555319@qq.com>
2017-09-15 18:41:25 +0800

    加入曲线显示,加入点击标记点，整理README

--- README.md
+++ README.md
@@ -1,28 +1,33 @@
-# NgGisDemo
+## NgGisDemo
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.2.
+> 百度一番没有找到相应整合的示例，好在官方做了支持  
+> 一个由Angular和Arcgis组建的一个基础地图demo
 
-## Development server
+## 快速开始
+环境：
+- nodejs > 6.9.x 
+- npm > 3.x.x
+- Angular CLI
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+获取源码(默认为dev分支):
+```
+# git clone http://git.oschina.net/StruggleYang/ngGisDemo
+```
+npm包下载
+```
+# cd ngGisDemo
 
-## Code scaffolding
+# npm install // 如果配置了cnpm建议使用cnpm
+```
+本地预览
+```
+# ng serve -o
+```
+通常预览在 http://localhost:4200/
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## 鸣谢
+- [Angular CLI](https://github.com/angular/angular-cli) version 1.3.2. 提供项目构建
+- [angular-esri-loader](https://github.com/tomwayson/angular-esri-loader) 提供在Angular中的Arcgis for JavaScript模块加载
+- [Arcgis for JavaScript](https://developers.arcgis.com/javascript/3/)
+- [Tom Wayson](https://github.com/tomwayson)
 
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-Before running the tests make sure you are serving the app via `ng serve`.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -6,21 +6,53 @@ import { EsriLoaderService } from 'angular-esri-loader';
   styleUrls: ['./esri-map.component.css']
 })
 export class EsriMapComponent implements OnInit {
+  // 这里指向一个选择器map在esri-map.component.html中
   @ViewChild('map') mapEl: ElementRef;
   // 地图初始变量
   map: any;
+  polylineJson: any = { // 标记线的json数据
+    'paths': [[
+        // 根据不同的地图坐标类型对应的坐标的值也会有变化
+        // [114.19699095146629, 30.603538511761222],
+    ]],
+    'spatialReference': { 'wkid' : 102100}
+};
+
+points: any = [ // 模拟的点数组数据用于描线
+    [12709568.097279584, 3581250.359213206],
+    [12708134.9029992, 3580175.4635029174],
+    [12707700.167400816, 3579950.9297323236],
+    [12707012.234146232, 3579864.9380755005],
+    [12706854.58277539, 3579812.3876185534],
+    [12706668.26751894, 3579611.7404192993],
+    [12706563.166605044, 3579415.8705343134],
+    [12706243.086549092, 3579520.971448208],
+    [12706362.519405792, 3579898.3792753764],
+    [12706610.939747725, 3580036.9213891467],
+    [12707461.30168742, 3580251.9005312044],
+    [12707590.289172653, 3580294.896359616],
+    [12708240.003913095, 3580667.526872516],
+    [12708794.172368176, 3581188.2541277227],
+    [12709424.777851546, 3581551.330012087],
+    [12709553.76533678, 3581269.4684702777]
+];
+/*******************地图***************/
 
   constructor(private esriLoader: EsriLoaderService) { }
 
   ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
     // only load the ArcGIS API for JavaScript when this component is loaded
     return this.esriLoader.load({
-      // use a specific version of the API instead of the latest
-      // url: '//js.arcgis.com/3.18/'
-      url: 'http://192.168.0.222/arcgis_js_api/library/3.17/3.17/init.js'
+      url: '//js.arcgis.com/3.17/'
+      // url: 'http://192.168.0.222/arcgis_js_api/library/3.17/3.17/init.js' // 如果没有搭建gis函数则需使用在线的
     }).then(() => {
-      // load the map class needed to create a new map
+      // 模块载入要一一对应
       this.esriLoader.loadModules([
+        'esri',
         'esri/map',
         'esri/layers/ArcGISTiledMapServiceLayer',
         'esri/toolbars/draw',
@@ -39,7 +71,7 @@ export class EsriMapComponent implements OnInit {
         'dojo',
         'dojo/_base/Color', 'dojo/dom', 'dojo/on', 'dojo/domReady!'
       ]).then(([
-        Map, ArcGISTiledMapServiceLayer, Draw,
+        esri, Map, ArcGISTiledMapServiceLayer, Draw,
         SimpleMarkerSymbol, SimpleLineSymbol,
         PictureMarkerSymbol, Point, Polyline,
         Extent, GraphicsLayer,
@@ -53,21 +85,30 @@ export class EsriMapComponent implements OnInit {
           zoom: 15, // 缩放比例
           logo: false, // 隐藏logo
           // slider:false // 隐藏侧边缩放工具
-          // basemap: 'osm'
+          // basemap: 'osm' // 不用基础图层
         });
         // 载入地图服务
-        const myTiledMapServiceLayer = new ArcGISTiledMapServiceLayer ('http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetColor/MapServer');
+        const myTiledMapServiceLayer = new ArcGISTiledMapServiceLayer (
+          'http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetColor/MapServer' );
         this.map.addLayer(myTiledMapServiceLayer);
 
         // dojo载入事件添加
         dojo.connect(this.map, 'onLoad', (evt) => {
-        // 点标记
-        const newPoint = new Point(12709568.097279584, 3581250.359213206, new SpatialReference({ wkid: 102100 }));
-        const picSymbol = new PictureMarkerSymbol('http://www.easyicon.net/api/resizeApi.php?id=1185658&size=32', 25, 32);
-        const graphic = new Graphic(newPoint, picSymbol);
-        // js通过add方法添加但typescript无效,
-        // ##原因：不能直接写在地图加载的后面，只能在dojo载入事件里写入,也就是这里
-        this.map.graphics.add(graphic);
+          // 点标记
+          const newPoint = new Point(12709568.097279584, 3581250.359213206, new SpatialReference({ wkid: 102100 }));
+          const picSymbol = new PictureMarkerSymbol('http://www.easyicon.net/api/resizeApi.php?id=1185658&size=32', 25, 32);
+          const graphic = new Graphic(newPoint, picSymbol);
+          this.map.graphics.add(graphic);
+
+          // 加入测试点数据
+          for (let i = 0; i < this.points.length; i++) {
+              this.polylineJson.paths[0].push([ this.points[i][0], this.points[i][1] ]);
+          }
+          const polyline = new Polyline(this.polylineJson);
+          // 线样式DASH虚线SOLID实线
+          const sys = new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASH, new esri.Color([16, 142, 233]), 3);
+          const graphic2 = new Graphic(polyline, sys);
+          this.map.graphics.add(graphic2); // 添加曲线
         });
 
         // dojo点击事件添加
@@ -76,11 +117,13 @@ export class EsriMapComponent implements OnInit {
           const cur_wkid = emp.spatialReference.wkid; // 当前坐标系
           console.log('当前地图的坐标系为:wkid==>' + cur_wkid);
           console.log('刚才点击的墨卡托为:x==>' + emp.x + '  y==>' + emp.y);
+          // 点击添加一个点
+          const newPoint = new Point(emp.x, emp.y, new SpatialReference({ wkid: 102100 }));
+          const picSymbol = new PictureMarkerSymbol('http://www.easyicon.net/api/resizeApi.php?id=1185658&size=32', 25, 32);
+          const graphic = new Graphic(newPoint, picSymbol);
+          this.map.graphics.add(graphic);
         });
       });
     });
   }
-
-
-  // 坐标换算方法
 }
